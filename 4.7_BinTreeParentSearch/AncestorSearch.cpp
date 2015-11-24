fcb8df5943eee7a0728afd1fcf53cd761780c488
rCori <ryancori1@gmail.com>
2015-11-23 17:00:21 -0800

    Finished off question 4.7 from CTCI. It asked to write an algorithm to find find the first common ancestor of two nodes in an unordered binary tree

--- 4.7_BinTreeParentSearch/AncestorSearch.cpp
+++ 4.7_BinTreeParentSearch/AncestorSearch.cpp
@@ -6,6 +6,177 @@
  * is correct.
  */
 
+#include "TreeNode.h"
+#include <iostream>
+
+template <class T>
+bool CommonAncestorTest(T expectedValue, TreeNode<T>* returnedNode,
+                        bool expectedNull = false){
+    //Make a return test success flag
+    bool testSuccess = false;
+    //State what the test is
+    std::cout << "Testing the retrieval of a common ancestor" 
+        << std::endl;
+    //Test for null
+    if(!returnedNode){
+        //If it is null and that was expected
+        if(expectedNull){
+            std::cout << "No node was returned as expected"
+                << std::endl;
+            //The test succeeded
+            testSuccess = true;
+        }
+        //If it was null and that is unexpected
+        else{
+            std::cout << "No node was returned indicating failure" 
+                << std::endl;
+        }
+        return testSuccess;
+    }
+    //Test value of node
+    T returnedValue = returnedNode->data;
+    //Relies of type T having an == operatator defined
+    if(expectedValue == returnedValue){
+        //print success message
+        std::cout << "Test Success!" << std::endl;
+        testSuccess = true;
+    }
+    //Test failed
+    else{
+        //print test failure message
+        std::cout << "Test failure" << std::endl;
+    }
+    return testSuccess;
+}
 
 int main(int argc, char** argv){
+    //Testing the find common ancestor function    
+    bool totalSuccess = true;
+
+    //Test1
+    //Arrange
+    //Make a tree by creating a head
+    TreeNode<int>* head = new TreeNode<int>();
+    head->data = 4;
+    //Act
+    head->left = new TreeNode<int>();
+    head->right = new TreeNode<int>();
+    head->left->data = 10;
+    head->right->data = -30;
+    TreeNode<int>* ancestor = 
+        head->FindFirstCommonAncestor(head->left,head->right);
+    
+    //Assert
+    //Test if the node found is what we are looking for
+    totalSuccess &= CommonAncestorTest<int>(4, ancestor);
+    delete head;
+
+    //Space between tests
+    std::cout << std::endl;    
+
+    //Test2
+    //Arrange
+    head = new TreeNode<int>();
+    head->data = 10;
+    //Act
+    TreeNode<int>* node1 = new TreeNode<int>();
+    node1->data = 20;
+    head->left = node1;
+
+    TreeNode<int>* node2 = new TreeNode<int>();
+    node2->data = 50;
+    head->right = node2;
+
+    TreeNode<int>* node3 = new TreeNode<int>();
+    node3->data = 30;
+    head->left->left = node3;
+    
+    TreeNode<int>* node4 = new TreeNode<int>();
+    node4->data = 700;
+    head->left->left->left = node4;
+    
+    TreeNode<int>* node5 = new TreeNode<int>();
+    node5->data = 100;
+    head->left->right = node5;
+   
+    ancestor = head->FindFirstCommonAncestor(node4, node5);
+    //Assert
+    //Test if the node found is what we are looking for
+    totalSuccess &= CommonAncestorTest<int>(20, ancestor);
+    delete head;
+   
+    //Space between tests
+    std::cout << std::endl;
+
+    //Test3
+    //Arrange
+    TreeNode<char>* charHead = new TreeNode<char>();
+    charHead->data = 'a';
+    //Act
+    TreeNode<char>* charNode1 = new TreeNode<char>();
+    charNode1->data = 'f';
+    charHead->left = charNode1;
+
+    TreeNode<char>* charNode2 = new TreeNode<char>();
+    charNode2->data = 'c';
+    charHead->right = charNode2;
+    
+    TreeNode<char>* charNode3 = new TreeNode<char>();
+    charNode3->data = 'm';
+    charHead->left->left = charNode3;
+
+    TreeNode<char>* charAncestor = charHead->FindFirstCommonAncestor
+                                             (charNode2, charNode3);
+    //Assert
+    //Test if the node found is what we are looking for
+    totalSuccess &= CommonAncestorTest<char>('a',  charAncestor);
+    delete charHead;
+
+    //Space between tests
+    std::cout << std::endl;
+    
+    //Test4
+    //Arrange
+    charHead = new TreeNode<char>();
+    charHead->data = 'q';
+    //Act
+    charNode1 = new TreeNode<char>();
+    charNode1->data = 'z';
+    charHead->left = charNode1;
+
+    charNode2 = new TreeNode<char>();
+    charNode2->data = 'a';
+    charHead->right = charNode2;
+
+    charNode3 = new TreeNode<char>();
+    charNode3->data = 'l';
+    charHead->left->left = charNode3;
+
+    TreeNode<char>* charNode4 = new TreeNode<char>();
+    charNode4->data = 'm';
+    charHead->left->right = charNode4;
+    
+    TreeNode<char>* charNode5 = new TreeNode<char>();
+    charNode5->data = 'n';
+    charHead->right->left = charNode5;
+
+    TreeNode<char>* charNode6 = nullptr;
+
+    //This will be null
+    charAncestor = charHead->FindFirstCommonAncestor
+                    (charNode3, charNode6);
+
+
+    //Test expects null
+    totalSuccess &= CommonAncestorTest<char>('q', charAncestor, true);
+
+    //See if all tests were successful or not
+    if(totalSuccess){
+        //print success message
+        std::cout << "All tests passed!" << std::endl;
+    }
+    else{
+        //print failure message
+        std::cout << "Some tests have failed" << std::endl;
+    }
 }
--- 4.7_BinTreeParentSearch/TreeNode.h
+++ 4.7_BinTreeParentSearch/TreeNode.h
@@ -21,15 +21,65 @@ public:
     TreeNode* left;
     //Right Child
     TreeNode* right;
+    //COnstructor just sets everything to nullptr
+    TreeNode(){
+        left = nullptr;
+        right = nullptr;
+    }
+    ~TreeNode(){
+        if(right){
+            delete right;
+        }
+        if(left){
+            delete left;
+        }
+    }
     //Search for specific data and return the node that contains it
     //If it does not exists returns nullptr
     TreeNode* Search(T searchData){
-        return nullptr;   
+        TreeNode* searchNode = nullptr;
+        //First check if this node is this node you are searching
+        //for
+        if(data == searchData){
+            searchNode = this;
+        }
+        //Search all children possible
+        TreeNode* leftNode = nullptr;
+        TreeNode* rightNode = nullptr;
+        if(left){
+            leftNode = left->Search(searchData);
+        }
+        if(right){
+            rightNode = right->Search(searchData);
+        }       
+        
+        //If either child contained the node we are searching for
+        //then return it.
+        if(rightNode)
+        {
+            searchNode = rightNode;
+        }
+        if(leftNode){
+            searchNode = leftNode;
+        }
+
+        //Return whatever we managed to find
+        return searchNode;
     }
     //Search for the first common ancestor tree node of two nodes
     //If it does not exist it returns nullptr
     TreeNode* FindFirstCommonAncestor(TreeNode* a, TreeNode* b){
-        return nullptr;
+        //Create a node to return
+        TreeNode* returnNode = nullptr;
+        //If either of the nodes are null, then obviously
+        //they have no common ancestor
+        if(!a || !b){
+            //Is null
+            return returnNode;
+        }
+        //call the recusrive function to start searching
+        returnNode = findAncestorRec(a, b);
+        return returnNode;
     }
 private:
     //Helper function for FindFirstCommonAncestor
@@ -39,6 +89,50 @@ private:
     //deaper in the tree, it will. If it is out of searching options
     //it will just return nullptr
     TreeNode* findAncestorRec(TreeNode* a, TreeNode* b){
+        //If node a is actually the node we are searching right now
+        //it could be the common ancestor if b is it's child
+        if(this == a){
+            if(Search(b->data)){
+                return a;
+            }
+        }
+        //Same check only for node b
+        if(this == b){
+            if(Search(a->data)){
+                return b;
+            }
+        }
+
+        //If we are not searching a or b, the only way this could
+        //be the ancestor node still is if a exists on one child
+        //subtree of this and b is on the other child subtree.
+        bool aOnLeft = false;
+        bool aOnRight = false;
+        bool bOnLeft = false;
+        bool bOnRight = false;
+        //See if a or b are on the left subtree if it exists
+        if(left){
+            aOnLeft = left->Search(a->data);
+            bOnLeft = left->Search(b->data);
+        }
+        if(right){
+            aOnRight = right->Search(a->data);
+            bOnRight = right->Search(b->data);
+        }
+        
+        //If we found a on one side and b on the other, then this is
+        //the ancestor we are looking for.
+        if((aOnRight && bOnLeft) || (bOnRight && aOnLeft)){
+            return this;
+        }
+        //if we did not find the two nodes split but found them
+        //on one side, we continue searching down that side if possible
+        else if(right && aOnRight && bOnRight){
+            return right->findAncestorRec(a,b);
+        }
+        else if(left && aOnLeft && bOnLeft){
+            return left->findAncestorRec(a,b);
+        }
         return nullptr;
     }
 };
