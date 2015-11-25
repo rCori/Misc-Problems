cbeeb832bcd54c35702db81126ed694bb54fae92
rCori <ryancori1@gmail.com>
2015-11-24 21:01:47 -0800

    Finished question 5.6 about the swapping even and odd bits in an integer. Functions works and test cases are verified in the single cpp file

--- 5.6_BitSwapEvenOdd/BitSwap.cpp
+++ 5.6_BitSwapEvenOdd/BitSwap.cpp
@@ -18,6 +18,17 @@
 int32_t BitSwapEvenOdd(int32_t num){
     //declare an int to return
     int32_t returnInt = -1;
+    //Shift right for the odd bit mask
+    int32_t rightShift = num >> 1;
+    //Shift left for the even bit mask
+    int32_t leftShift = num << 1;
+    //Make odd bitmask
+    //0101 = 5
+    int32_t oddBitmask = 0x55555555;
+    //Make even bitmask
+    //1010 = a
+    int32_t evenBitmask = 0xaaaaaaaa;
+    returnInt = (rightShift & oddBitmask) | (leftShift & evenBitmask);
     return returnInt;
 }
 
@@ -47,5 +58,38 @@ bool TestBitSwap(int32_t input, int32_t expected){
 }
 
 int main(int argc, char** argv){
+    //Start testing BitSwapEvenOdd
+    bool allTestsPass = true;
+
+    //22     = 0x00 00 00 00 00 01 01 10
+    //41     = 0x00 00 00 00 00 10 10 01
+    allTestsPass &= TestBitSwap(22,41);
     
+    // 1161  = 0x00 00 01 00 10 00 10 01
+    // 2118  = 0x00 00 10 00 01 00 01 10
+    allTestsPass &= TestBitSwap(1161 , 2118);
+
+    // 8237  = 0x00 10 00 00 00 10 11 01
+    // 4126  = 0x00 01 00 00 00 01 11 10
+    allTestsPass &= TestBitSwap(8237, 4126); 
+
+    // 729   = 0x00 00 00 10 11 01 10 01
+    // 486   = 0x00 00 00 01 11 10 01 10
+    allTestsPass &= TestBitSwap(729, 486);
+
+    // 19886 = 0x01 00 11 01 10 10 11 10
+    // 36445 = 0x10 00 11 10 01 01 11 01
+    allTestsPass &= TestBitSwap(19886, 36445);
+
+    // 341   = 0x00 00 00 01 01 01 01 01
+    // 682   = 0x00 00 00 10 10 10 10 10
+    allTestsPass &= TestBitSwap(341, 682);
+
+    //Print message for all tests passing or not
+    if(allTestsPass){
+        std::cout << "All tests have passed" << std::endl;
+    }
+    else{
+        std::cout << "Some tests have failed" << std::endl;
+    } 
 }
